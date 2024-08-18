fc3ae80335e91f9e4ba066c529f81aca62ee7f97
이효민 <ihyomin@ihyomin-ui-MacBookAir.local>
2024-08-18 14:28:46 +0900

    Feat:add routing dart file

---
+++ lib/router.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+import 'screen/chat_screen.dart';
+import 'screen/adot_call.dart';
+import 'screen/loading_screen.dart';
+import 'screen/message_summary.dart';
+import 'screen/message_summary_screen.dart';
+import 'screen/schedule_register_screen.dart';
+import 'screen/summary_screen1.dart';
+import 'screen/summary_screen2.dart';
+import 'screen/tmap.dart';
+import 'screen/warning_screen.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+        path: '/',
+        builder: (context, state) => ChatScreen()),
+    GoRoute(
+        path: '/tmap',
+        builder: (context, state) => PhoneConnectionScreen()),
+  ],
+);
