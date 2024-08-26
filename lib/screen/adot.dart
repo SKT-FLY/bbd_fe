fa6fcec7a9cd6fd21462d98b8c2b59312c851315
daxnii <stgyals3135@hanyang.ac.kr>
2024-08-27 01:18:37 +0900

    Fix:렌더링 에러 해결

--- lib/screen/adot.dart
+++ lib/screen/adot.dart
@@ -40,14 +40,8 @@ class _AdotScreenState extends State<AdotScreen> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
     // 화면의 크기 정보 가져오기
-    final screenWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
-    final screenHeight = MediaQuery
-        .of(context)
-        .size
-        .height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
 
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
@@ -56,8 +50,6 @@ class _AdotScreenState extends State<AdotScreen> with SingleTickerProviderStateM
         leading: CupertinoButton(
           padding: EdgeInsets.zero,
           onPressed: () => _selectUser(context, 1),
-          // 좌측 아이콘 클릭 시 수행할 작업
-
           child: CustomPaint(
             size: Size(24, 24), // 아이콘의 크기를 설정
             painter: MenuIconPainter(), // CustomPainter로 아이콘을 그림
@@ -66,7 +58,6 @@ class _AdotScreenState extends State<AdotScreen> with SingleTickerProviderStateM
         trailing: CupertinoButton(
           padding: EdgeInsets.zero,
           onPressed: () => _selectUser(context, 3),
-          // 우측 아이콘 클릭 시 수행할 작업
           child: CustomPaint(
             size: Size(24, 24), // 아이콘의 크기를 설정
             painter: DotsIconPainter(), // CustomPainter로 점 세 개 아이콘을 그림
@@ -79,84 +70,76 @@ class _AdotScreenState extends State<AdotScreen> with SingleTickerProviderStateM
             flex: 1,
             child: Padding(
               padding: EdgeInsets.all(screenWidth * 0.05), // 화면 크기의 5%를 패딩으로 설정
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start, // 콘텐츠를 최상단에 위치시킴
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '깊은 밤이에요. 오늘 하루를 마무리하는\n편안한 시간 보내세요.',
-                          style: TextStyle(
-                            fontSize: screenWidth * 0.06,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: screenHeight * 0.02),
-                        CupertinoButton(
-                          onPressed: () {},
-                          color: CupertinoColors.systemGrey6,
-                          padding: EdgeInsets.symmetric(
-                            vertical: screenHeight * 0.02,
-                          ),
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              '잠이 안 올 때는 어떻게 해야 돼?',
-                              style: TextStyle(
-                                color: CupertinoColors.black,
-                                fontSize: screenWidth * 0.045,
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: screenHeight * 0.01),
-                        CupertinoButton(
-                          onPressed: () {},
-                          color: CupertinoColors.systemGrey6,
-                          padding: EdgeInsets.symmetric(
-                            vertical: screenHeight * 0.02,
-                          ),
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              '빵을 잘 굽는 방법 알려줘',
-                              style: TextStyle(
-                                color: CupertinoColors.black,
-                                fontSize: screenWidth * 0.045,
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: screenHeight * 0.01),
-                        CupertinoButton(
-                          onPressed: () {},
-                          color: CupertinoColors.systemGrey6,
-                          padding: EdgeInsets.symmetric(
-                            vertical: screenHeight * 0.02,
-                          ),
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              '아보카도의 효능은 뭐야?',
-                              style: TextStyle(
-                                color: CupertinoColors.black,
-                                fontSize: screenWidth * 0.045,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+              child: SingleChildScrollView( // SingleChildScrollView로 감싸서 스크롤 가능하게 설정
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '깊은 밤이에요. 오늘 하루를 마무리하는\n편안한 시간 보내세요.',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.06,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: screenHeight * 0.02),
+                    CupertinoButton(
+                      onPressed: () {},
+                      color: CupertinoColors.systemGrey6,
+                      padding: EdgeInsets.symmetric(
+                        vertical: screenHeight * 0.02,
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '잠이 안 올 때는 어떻게 해야 돼?',
+                          style: TextStyle(
+                            color: CupertinoColors.black,
+                            fontSize: screenWidth * 0.045,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.01),
+                    CupertinoButton(
+                      onPressed: () {},
+                      color: CupertinoColors.systemGrey6,
+                      padding: EdgeInsets.symmetric(
+                        vertical: screenHeight * 0.02,
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '빵을 잘 굽는 방법 알려줘',
+                          style: TextStyle(
+                            color: CupertinoColors.black,
+                            fontSize: screenWidth * 0.045,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.01),
+                    CupertinoButton(
+                      onPressed: () {},
+                      color: CupertinoColors.systemGrey6,
+                      padding: EdgeInsets.symmetric(
+                        vertical: screenHeight * 0.02,
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '아보카도의 효능은 뭐야?',
+                          style: TextStyle(
+                            color: CupertinoColors.black,
+                            fontSize: screenWidth * 0.045,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-
-          Spacer(),
           Expanded(
             flex: 1,
             child: Column(
@@ -170,7 +153,9 @@ class _AdotScreenState extends State<AdotScreen> with SingleTickerProviderStateM
                       width: screenWidth * 0.2, // 화면 너비의 20%로 아이콘 크기 설정
                       height: screenWidth * 0.2, // 화면 너비의 20%로 아이콘 크기 설정
                     ),
-                    onPressed: () => _selectUser(context, 2),
+                    onPressed: () {
+                      // 버튼 클릭 시 실행할 기능 추가
+                    },
                   ),
                 ),
                 Spacer(), // 빈 공간을 추가하여 마이크 버튼을 아래로 밀어냄
@@ -208,71 +193,11 @@ class _AdotScreenState extends State<AdotScreen> with SingleTickerProviderStateM
               ],
             ),
           ),
-
-          // Expanded(
-          //   flex: 1,
-          //   child: Column(
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: [
-          //       FadeTransition(
-          //         opacity: _animation,
-          //         child: CupertinoButton(
-          //           child: Image.asset(
-          //             'assets/images/click_button.png',
-          //             width: screenWidth * 0.2, // 화면 너비의 20%로 아이콘 크기 설정
-          //             height: screenWidth * 0.2, // 화면 너비의 20%로 아이콘 크기 설정
-          //           ),
-          //           onPressed: () {
-          //             // 버튼 클릭 시 실행할 기능 추가
-          //           },
-          //         ),
-          //       ),
-          //       SizedBox(height: screenHeight * 0.02), // 간격 추가
-          //       Padding(
-          //         padding: EdgeInsets.all(screenWidth * 0.02),
-          //         // 화면 크기의 2%를 패딩으로 설정
-          //         child: Row(
-          //           children: [
-          //             Expanded(
-          //               child: CupertinoTextField(
-          //                 placeholder: '궁금하거나 필요한 것을 말씀해 주세요',
-          //                 padding: EdgeInsets.symmetric(
-          //                   vertical: screenHeight * 0.015,
-          //                   // 화면 높이의 1.5%를 패딩으로 설정
-          //                   horizontal: screenWidth *
-          //                       0.025, // 화면 너비의 2.5%를 패딩으로 설정
-          //                 ),
-          //                 decoration: BoxDecoration(
-          //                   border: Border.all(
-          //                       color: CupertinoColors.systemGrey),
-          //                   borderRadius: BorderRadius.circular(10.0),
-          //                 ),
-          //               ),
-          //             ),
-          //             CupertinoButton(
-          //               padding: EdgeInsets.all(screenWidth * 0.025),
-          //               // 화면 크기의 2.5%를 패딩으로 설정
-          //               child: Icon(
-          //                 CupertinoIcons.mic,
-          //                 size: screenWidth * 0.06, // 아이콘 크기를 화면 너비의 6%로 설정
-          //                 color: CupertinoColors.black, // 아이콘 색상을 검정으로 설정
-          //               ),
-          //               onPressed: () {
-          //                 // 마이크 버튼 기능 추가
-          //               },
-          //             ),
-          //           ],
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
         ],
       ),
     );
   }
 }
-
 
 // CustomPainter를 사용하여 원하는 스타일의 메뉴 아이콘을 그립니다.
 class MenuIconPainter extends CustomPainter {
@@ -286,7 +211,7 @@ class MenuIconPainter extends CustomPainter {
     // 첫 번째 줄
     canvas.drawLine(
       Offset(0, size.height * 0.3),
-      Offset(size.width*0.8, size.height * 0.3),
+      Offset(size.width * 0.8, size.height * 0.3),
       paint,
     );
     // 두 번째 줄 (중간 줄, 더 짧게, 왼쪽에 맞춤)
@@ -295,17 +220,10 @@ class MenuIconPainter extends CustomPainter {
       Offset(size.width * 0.6, size.height * 0.5),
       paint,
     );
-    // // 두 번째 줄 (중간 줄, 더 짧게)
-    // canvas.drawLine(
-    //   Offset(size.width * 0.2, size.height * 0.5),
-    //   Offset(size.width * 0.8, size.height * 0.5),
-    //   paint,
-    // );
-
     // 세 번째 줄
     canvas.drawLine(
       Offset(0, size.height * 0.7),
-      Offset(size.width*0.8, size.height * 0.7),
+      Offset(size.width * 0.8, size.height * 0.7),
       paint,
     );
   }
@@ -315,6 +233,7 @@ class MenuIconPainter extends CustomPainter {
     return false;
   }
 }
+
 // 세 개의 점을 그리는 CustomPainter
 class DotsIconPainter extends CustomPainter {
   @override
@@ -351,5 +270,3 @@ class DotsIconPainter extends CustomPainter {
     return false;
   }
 }
-
-
