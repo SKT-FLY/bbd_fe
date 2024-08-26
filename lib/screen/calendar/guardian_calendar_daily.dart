d0ffdd09cb20bc29a113cc8f53c8a6b7b83e830c
daxnii <stgyals3135@hanyang.ac.kr>
2024-08-26 23:48:08 +0900

    feat:보호자 캘린더 탭 추가

---
+++ lib/screen/calendar/guardian_calendar_daily.dart
@@ -0,0 +1,436 @@
+import 'dart:typed_data';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:bbd_project_fe/setting/api_service.dart';
+import 'package:go_router/go_router.dart';
+import 'package:bbd_project_fe/setting/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:logger/logger.dart';
+
+import '../../setting/api_service.dart';
+import '../../setting/user_provider.dart';
+
+class GuardianScheduleDailyScreen extends StatefulWidget {
+  final DateTime selectedDate;
+  //final Map<String, dynamic>? extraData;
+  final List<dynamic>? extraData;
+
+  const GuardianScheduleDailyScreen({
+    Key? key,
+    required this.selectedDate,
+    this.extraData,
+  }) : super(key: key);
+
+  @override
+  _ScheduleDailyScreenState createState() => _ScheduleDailyScreenState();
+}
+
+class _ScheduleDailyScreenState extends State<GuardianScheduleDailyScreen> {
+  late int _selectedYear;
+  late int _selectedMonth;
+  late int _selectedDay;
+  final ScrollController _scrollController = ScrollController();
+  late Future<List<dynamic>> _scheduleDataFuture;
+  final ApiService _apiService = ApiService();
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  final Logger _logger = Logger();
+  List<dynamic> _schedules = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedYear = widget.selectedDate.year;
+    _selectedMonth = widget.selectedDate.month;
+    _selectedDay = widget.selectedDate.day;
+    print("투데이 진입완료"+_selectedYear.toString()+_selectedMonth.toString()+_selectedDay.toString());
+    _scheduleDataFuture = _fetchScheduleData();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToSelectedDay();
+    });
+
+    _fetchAndPlaySchedule();  // 음성 재생 기능 추가
+  }
+
+  Future<void> _fetchAndPlaySchedule() async {
+    final userId = Provider.of<UserProvider>(context, listen: false).userId;
+    final date = '${_selectedYear}-${_selectedMonth}-${_selectedDay}';
+
+    // 이전에 재생 중이던 음성을 정지
+    await _audioPlayer.stop();
+
+    final result = await _apiService.fetchSchedule(date, userId);
+
+    if (result['status'] == 'success') {
+      if (result.containsKey('data')) {
+        Uint8List audioBytes = Uint8List.fromList(result['data']);
+        await _audioPlayer.play(BytesSource(audioBytes));
+        _logger.i('Audio is playing.');
+      } else if (result.containsKey('message')) {
+        String message = result['message'];
+        _logger.i('Received message: $message');
+        // 메시지를 화면에 표시하거나 다른 작업 수행
+      }
+    } else {
+      _logger.e('Error: ${result['message'] ?? result['exception']}');
+    }
+  }
+
+  Future<List<dynamic>> _fetchScheduleData() async {
+    try {
+      final userId = Provider.of<UserProvider>(context, listen: false).userId;
+      final List<dynamic> schedules = await _apiService.fetchGuardianSchedules(userId);
+      print("스케줄은...");
+      print(schedules);
+      setState(() {
+        _schedules = schedules;
+      });
+      return schedules;
+    } catch (e) {
+      _logger.e('Error fetching schedules: $e');
+      return [];
+    }
+  }
+
+  void _scrollToSelectedDay() {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double targetOffset = (_selectedDay - 1) * (screenWidth / 5) - (screenWidth / 2) + (screenWidth / 10);
+    _scrollController.animateTo(
+      targetOffset,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  bool _hasEventForDay(DateTime day) {
+    return _schedules.any((schedule) {
+      final scheduleDate = DateTime.parse(schedule['schedule_start_time']).toLocal();
+      return scheduleDate.year == day.year &&
+          scheduleDate.month == day.month &&
+          scheduleDate.day == day.day;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int daysInMonth = _daysInMonth(_selectedYear, _selectedMonth);
+    double boxWidth = MediaQuery.of(context).size.width / 5 - 8;
+    double boxHeight = 100;
+    double selectedBoxHeight = 120;
+
+    return CupertinoPageScaffold(
+      child: Stack(
+        children: [
+          SafeArea(
+            child: Column(
+              children: [
+                _buildMonthSelector(),
+                const SizedBox(height: 16),
+                _buildDaySelector(
+                    daysInMonth, boxWidth, boxHeight, selectedBoxHeight),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 4.0),
+                  child: Divider(
+                    thickness: 1,
+                    color: CupertinoColors.systemGrey,
+                  ),
+                ),
+                Expanded(
+                  child: FutureBuilder<List<dynamic>>(
+                    future: _scheduleDataFuture,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CupertinoActivityIndicator());
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('Error: ${snapshot.error}'));
+                      } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                        return _buildScheduleList(snapshot.data!);
+                      } else {
+                        return const Center(
+                          child: Text(
+                            '일정이 없습니다.',
+                            style: TextStyle(
+                                fontSize: 20, color: CupertinoColors.systemGrey),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: CupertinoButton(
+                padding: EdgeInsets.zero,
+                onPressed: () async {
+                  await _audioPlayer.stop();
+                  final userId = Provider.of<UserProvider>(context, listen: false).userId;
+                  context.go('/guardian-monthly-schedule', extra: userId);
+                },
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: CupertinoColors.systemYellow,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: CupertinoColors.black.withOpacity(0.25),
+                        spreadRadius: 1,
+                        blurRadius: 6,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: const Icon(
+                    CupertinoIcons.calendar,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMonthSelector() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CupertinoButton(
+          child: const Icon(CupertinoIcons.left_chevron),
+          onPressed: () {
+            setState(() {
+              if (_selectedMonth > 1) {
+                _selectedMonth--;
+              } else {
+                _selectedMonth = 12;
+                _selectedYear--;
+              }
+              _selectedDay = 1;
+            });
+            _scrollToSelectedDay();
+            _fetchAndPlaySchedule();  // 날짜 변경 시 새로운 음성 재생
+          },
+        ),
+        Text(
+          '$_selectedYear년 $_selectedMonth월',
+          style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+        CupertinoButton(
+          child: const Icon(CupertinoIcons.right_chevron),
+          onPressed: () {
+            setState(() {
+              if (_selectedMonth < 12) {
+                _selectedMonth++;
+              } else {
+                _selectedMonth = 1;
+                _selectedYear++;
+              }
+              _selectedDay = 1;
+            });
+            _scrollToSelectedDay();
+            _fetchAndPlaySchedule();  // 날짜 변경 시 새로운 음성 재생
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDaySelector(int daysInMonth, double boxWidth, double boxHeight, double selectedBoxHeight) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      controller: _scrollController,
+      child: Row(
+        children: List.generate(daysInMonth, (index) {
+          int day = index + 1;
+          String weekDay = _getWeekDay(_selectedYear, _selectedMonth, day);
+          DateTime currentDay = DateTime(_selectedYear, _selectedMonth, day);
+          bool hasEvent = _hasEventForDay(currentDay);
+          bool isSelected = day == _selectedDay;
+
+          return GestureDetector(
+            onTap: () async {
+              await _audioPlayer.stop();  // 기존 음성을 정지
+              setState(() {
+                _selectedDay = day;
+              });
+              _scrollToSelectedDay();
+              _fetchAndPlaySchedule();  // 새 날짜의 음성 재생
+            },
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minWidth: boxWidth,
+                maxWidth: boxWidth + 8,
+              ),
+              child: _buildCalendarDate(
+                day.toString(),
+                weekDay,
+                hasEvent,
+                isSelected,
+                boxWidth,
+                isSelected ? selectedBoxHeight : boxHeight,
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget _buildScheduleList(List<dynamic> scheduleList) {
+    final filteredSchedules = scheduleList.where((schedule) {
+      final scheduleDate = DateTime.parse(schedule['schedule_start_time']).toLocal();
+      return scheduleDate.year == _selectedYear &&
+          scheduleDate.month == _selectedMonth &&
+          scheduleDate.day == _selectedDay;
+    }).toList();
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: ListView.builder(
+        itemCount: filteredSchedules.length,
+        itemBuilder: (context, index) {
+          final schedule = filteredSchedules[index];
+          return _buildScheduleCard(
+            time: schedule['schedule_start_time'] ?? 'N/A',
+            title: schedule['schedule_name'] ?? 'N/A',
+            description: schedule['schedule_description'] ?? 'N/A',
+          );
+        },
+      ),
+    );
+  }
+
+  int _daysInMonth(int year, int month) {
+    return DateTime(year, month + 1, 0).day;
+  }
+
+  String _getWeekDay(int year, int month, int day) {
+    DateTime date = DateTime(year, month, day);
+    const weekDays = ['일', '월', '화', '수', '목', '금', '토'];
+    return weekDays[date.weekday % 7];
+  }
+
+  Widget _buildCalendarDate(String day, String weekDay, bool hasEvent, bool isSelected, double width, double height) {
+    return Container(
+      width: width,
+      height: height,
+      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: Column(
+        children: [
+          Container(
+            width: width,
+            height: 16,
+            decoration: BoxDecoration(
+              color: isSelected ? const Color(0xFFFFC600) : (hasEvent ? Colors.lightGreenAccent : CupertinoColors.systemGrey),
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              width: width,
+              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+              decoration: BoxDecoration(
+                color: CupertinoColors.white,
+                borderRadius: const BorderRadius.vertical(bottom: Radius.circular(12)),
+                boxShadow: [
+                  BoxShadow(
+                    color: CupertinoColors.black.withOpacity(0.1),
+                    blurRadius: 6,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+                border: Border.all(
+                  color: isSelected ? Colors.transparent : (hasEvent ? Colors.lightGreenAccent : Colors.transparent),
+                  width: isSelected ? 0.0 : 3.0,
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    weekDay,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: isSelected ? CupertinoColors.activeOrange : CupertinoColors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  Text(
+                    day,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: isSelected ? CupertinoColors.activeOrange : CupertinoColors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildScheduleCard({
+    required String time,
+    required String title,
+    required String description,
+  }) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.only(top: 10.0),
+      decoration: BoxDecoration(
+        color: CupertinoColors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: CupertinoColors.black.withOpacity(0.1),
+            blurRadius: 12,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            time,
+            style: const TextStyle(
+              fontSize: 24,
+              color: CupertinoColors.inactiveGray,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            description,
+            style: const TextStyle(
+              fontSize: 24,
+              color: CupertinoColors.systemGrey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/calendar/guardian_calendar_monthly.dart
@@ -0,0 +1,364 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:go_router/go_router.dart';
+import 'package:bbd_project_fe/setting/api_service.dart';
+
+import '../../setting/user_provider.dart';
+
+class GuardianScheduleMonthlyScreen extends StatefulWidget {
+  //final int userId;
+
+  const GuardianScheduleMonthlyScreen({super.key});
+    //required this.userId});
+
+  @override
+  _GuardianScheduleMonthlyScreenState createState() =>
+      _GuardianScheduleMonthlyScreenState();
+}
+
+class _GuardianScheduleMonthlyScreenState
+    extends State<GuardianScheduleMonthlyScreen> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay = DateTime.now();
+  final ApiService _apiService = ApiService(); // ApiService 인스턴스 생성
+  late Future<List<dynamic>> _scheduleDataFuture;
+  Map<DateTime, List<dynamic>> _events = {};
+  Map<int, Color> _userColors = {};
+  //late int guardianId;
+
+  @override
+  void initState() {
+    super.initState();
+    //guardianId = widget.userId;
+    //guardianId =
+    _fetchGuardianSchedules();
+  }
+
+  void _fetchGuardianSchedules() {
+    final guardianId = Provider.of<UserProvider>(context, listen: false).userId;
+    setState(() {
+      print("가디언 페이지");
+      //final guardianId = Provider.of<UserProvider>(context, listen: false).userId;
+      _scheduleDataFuture = _apiService.fetchGuardianSchedules(guardianId);
+      _scheduleDataFuture.then((schedules) {
+        setState(() {
+          _events = _groupEventsByDate(schedules);
+        });
+        // _scheduleDataFuture.then((events) {
+        //   setState(() {
+        //     _events = events;
+        //     _assignColorsToUsers();
+        //   });
+      }).catchError((error) {
+        print('Error fetching schedules: $error');
+      });
+    });
+  }
+
+  Map<DateTime, List<dynamic>> _groupEventsByDate(List<dynamic> schedules) {
+    Map<DateTime, List<dynamic>> events = {};
+    for (var schedule in schedules) {
+      DateTime date = DateTime.parse(schedule['schedule_start_time']).toLocal();
+      final dateOnly = DateTime(date.year, date.month, date.day);
+      if (events[dateOnly] == null) {
+        events[dateOnly] = [];
+      }
+      events[dateOnly]!.add(schedule);
+    }
+    return events;
+  }
+
+  void _assignColorsToUsers() {
+    int colorIndex = 0;
+    List<Color> colors = [
+      Colors.red,
+      Colors.blue,
+      Colors.green,
+      Colors.orange,
+      Colors.purple,
+      Colors.cyan,
+      Colors.amber
+    ]; // 피보호자별 색상 지정Future<List<dynamic>>
+
+    _events.forEach((date, schedules) {
+      for (var schedule in schedules) {
+        int userId = schedule['user_id'];
+        if (!_userColors.containsKey(userId)) {
+          _userColors[userId] = colors[colorIndex % colors.length];
+          colorIndex++;
+        }
+      }
+    });
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    //print("날짜 선택"+selectedDay.toString()+_events.toString()+guardianId.toString());
+    final guardianId = Provider.of<UserProvider>(context, listen: false).userId;
+    setState(() {
+      _selectedDay = selectedDay;
+      _focusedDay = focusedDay;
+    });
+
+    final selectedEvents = _events[selectedDay] ?? [];
+    context.go('/guardian-daily-schedule', extra: {
+      'selectedDate': selectedDay,
+      'events': selectedEvents,
+      'userId': guardianId,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final guardianId = Provider.of<UserProvider>(context, listen:false).userId;
+    return CupertinoPageScaffold(
+      navigationBar: null,
+      child: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              flex: 2,
+              child: _buildMonthSelector(),
+            ),
+            const SizedBox(height: 10),
+            Flexible(
+              flex: 14,
+              child: FutureBuilder<List<dynamic>>(
+                future: _scheduleDataFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CupertinoActivityIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (snapshot.hasData) {
+                    return _buildCalendar();
+                  } else {
+                    return const Center(child: Text('일정이 없습니다.'));
+                  }
+                },
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Center(
+                child: CupertinoButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () {
+                    context.go('/chat');
+                  },
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: CupertinoColors.systemYellow,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: CupertinoColors.black.withOpacity(0.25),
+                          spreadRadius: 1,
+                          blurRadius: 6,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: const Icon(
+                      CupertinoIcons.home,
+                      color: CupertinoColors.white,
+                      size: 50,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMonthSelector() {
+    final guardianId = Provider.of<UserProvider>(context, listen: false).userId;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CupertinoButton(
+          child: const Icon(CupertinoIcons.left_chevron, size: 30),
+          onPressed: () {
+            setState(() {
+              _focusedDay = DateTime(
+                _focusedDay.year,
+                _focusedDay.month - 1,
+                1,
+              );
+              if (_focusedDay.month == 0) {
+                _focusedDay = DateTime(_focusedDay.year - 1, 12, 1);
+              }
+              _selectedDay = _focusedDay;
+            });
+          },
+        ),
+        Text(
+          '${_focusedDay.year}년 ${_focusedDay.month}월',
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        CupertinoButton(
+          child: const Icon(CupertinoIcons.right_chevron, size: 30),
+          onPressed: () {
+            setState(() {
+              _focusedDay = DateTime(
+                _focusedDay.year,
+                _focusedDay.month + 1,
+                1,
+              );
+              if (_focusedDay.month == 13) {
+                _focusedDay = DateTime(_focusedDay.year + 1, 1, 1);
+              }
+              _selectedDay = _focusedDay;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCalendar() {
+    return TableCalendar<dynamic>(
+      firstDay: DateTime.utc(2010, 1, 1),
+      lastDay: DateTime.utc(2030, 12, 31),
+      focusedDay: _focusedDay,
+      selectedDayPredicate: (day) {
+        return isSameDay(_selectedDay, day);
+      },
+      onDaySelected: _onDaySelected,
+      calendarFormat: CalendarFormat.month,
+      eventLoader: (day) {
+        final dateOnly = DateTime(day.year, day.month, day.day);
+        return _events[dateOnly] ?? [];
+      },
+      daysOfWeekStyle: const DaysOfWeekStyle(
+        weekdayStyle: TextStyle(fontSize: 24, color: Colors.black),
+        weekendStyle: TextStyle(fontSize: 24, color: Colors.red),
+      ),
+      daysOfWeekHeight: 40.0,
+      calendarStyle: CalendarStyle(
+        todayDecoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: Colors.black,
+            width: 2.0,
+          ),
+        ),
+        selectedDecoration: const BoxDecoration(
+          color: CupertinoColors.black,
+          shape: BoxShape.circle,
+        ),
+        outsideDaysVisible: false,
+        cellMargin: const EdgeInsets.all(6.0),
+        defaultTextStyle: const TextStyle(fontSize: 24),
+      ),
+      rowHeight: 100.0,
+      calendarBuilders: CalendarBuilders<dynamic>(
+        dowBuilder: (context, day) {
+          if (day.weekday == DateTime.saturday) {
+            return const Center(
+              child: Text(
+                '토',
+                style: TextStyle(fontSize: 24, color: Colors.blue),
+              ),
+            );
+          } else if (day.weekday == DateTime.sunday) {
+            return const Center(
+              child: Text(
+                '일',
+                style: TextStyle(fontSize: 24, color: Colors.red),
+              ),
+            );
+          }
+          return null;
+        },
+        defaultBuilder: (context, day, focusedDay) {
+          final dateOnly = DateTime(day.year, day.month, day.day);
+          final hasEvents = _events[dateOnly]?.isNotEmpty ?? false;
+          final events = _events[dateOnly] ?? [];
+
+          return Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0), // 상단에서 8.0 여백을 줌
+                  child: Text(
+                    '${day.day}',
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: day.weekday == DateTime.sunday
+                          ? Colors.red
+                          : day.weekday == DateTime.saturday
+                          ? Colors.blue
+                          : Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+
+              if (events.isNotEmpty)
+
+              Positioned(
+                top: 36, // 숫자 아래로 여유를 둠
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: List.generate(events.length, (index) {
+                    int userId = events[index]['user_id'];
+                    String scheduleName = events[index]['schedule_name'];
+
+                    // 제목이 6글자를 넘을 경우 잘라서 표시
+                    if (scheduleName.length > 6) {
+                      scheduleName = scheduleName.substring(0, 6) + '...';
+                    }
+
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 1.0), // 네모 사이의 간격을 더 넓게
+                      child: Container(
+                        width: 90, // 직사각형의 너비를 지정
+                        height: 15, // 직사각형의 높이를 지정
+                        decoration: BoxDecoration(
+                          color: _userColors[userId]?.withOpacity(0.8) ?? Colors.black.withOpacity(0.8),
+                          borderRadius: BorderRadius.circular(20.0), // 둥근 모서리 적용
+                        ),
+                        child: Center(
+                          child: Text(
+                            scheduleName,
+                            style: TextStyle(
+                              color: Colors.white, // 텍스트 색상
+                              fontSize: 10, // 작은 글씨 크기
+                            ),
+                            maxLines: 1, // 한 줄로 표시
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+
+            ],
+          );
+        },
+
+        markerBuilder: (context, day, events) {
+          return Container();
+        },
+      ),
+      locale: 'ko_KR',
+      headerVisible: false,
+      startingDayOfWeek: StartingDayOfWeek.sunday,
+      onPageChanged: (focusedDay) {
+        setState(() {
+          _focusedDay = focusedDay;
+        });
+      },
+    );
+  }
+}
